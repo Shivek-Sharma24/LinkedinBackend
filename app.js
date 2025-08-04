1d9ae3b65dfab34aee907c2c2c9da136abd8bce8
Shivek-Sharma24 <shiveksharma24@gmail.com>
2025-08-04 21:31:34 +0530

    change

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-let createError = require('http-errors');
+// let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-// const expressSession = require("express-session")
+// let cookieParser = require('cookie-parser');
+// let logger = require('morgan');
 const cors = require('cors')
-// const flash = require("connect-flash")
 let indexRouter = require('./routes/index');
- let usersRouter = require('./routes/users');
+// let usersRouter = require('./routes/users');
 let port = 8000;
-// const passport = require("passport")
-
 var app = express();
 app.listen(port,()=>{
   console.log(`App running on http://localhost:${port}`)
@@ -23,47 +19,41 @@ app.use(
   })
 );
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// app.use(flash())
-// app.use(expressSession(
-//   {
-//     resave:false,
-//     saveUninitialized:false , 
-//     secret:'shivek'
-//   }
-// ));
-// app.use(passport.initialize());
-
-// passport.serializeUser(usersRouter.serializeUser());
-// passport.deserializeUser(usersRouter.deserializeUser());
-// app.use(passport.session());
-
-
-app.use(logger('dev'));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.mongodb_url, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,7 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-mongoose
-  .connect(process.env.mongodb_url)
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((err) => {
-    console.log("Databse Errror", err);
-  });
-// MongoDB_Url= mongodb+srv://shivek_24:shiveksharma8755@shivek.6yh3v.mongodb.net/FoodReceipe
-// secretKey = Shivekrecipe24
-
-// const plm = require("passport-local-mongoose");
-
 const userSchema = mongoose.Schema({
   username: String,
-  // name: String,
   email: String,
-  // contact : Number,
   password: String,
   profileimage: String,
 
@@ -29,7 +12,5 @@ const userSchema = mongoose.Schema({
     },
   ],
 });
-
-// userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
